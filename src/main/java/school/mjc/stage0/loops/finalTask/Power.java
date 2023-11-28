9be9dc54cadada83c588deed4f52bd5051f7f3ea
rqhappyy <dtereschenko110@gmail.com>
2023-11-28 15:36:32 +0300

    final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,8 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int result = (int)Math.pow(numberToPrint, power);
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int center = cathetusLength-1;
+        for (int i = 0; i < cathetusLength; i++) {
+            int k = i+1;
+            for (int j = 0; j < cathetusLength*2; j++) {
+                if(i<=center) {
+                    if(j>=center-i&&j<=center+i){
+                        System.out.print(k);
+                        if(j<center){
+                            k--;
+                        }else{
+                            k++;
+                        }
+                    }else if(j<center-i){
+                        System.out.print(" ");
+                    }
+                }
+            }
+                System.out.print("\n");
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
     }
 }
